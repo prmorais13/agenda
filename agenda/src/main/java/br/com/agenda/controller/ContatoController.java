b5695e6a1124c637dca8db1c8ca2f26dc213d5e6
prmorais13 <pauloroberto@natal.rn.gov.br>
2017-02-01 23:23:47 -0300

    Segundo commit - casa

--- agenda/src/main/java/br/com/agenda/controller/ContatoController.java
+++ agenda/src/main/java/br/com/agenda/controller/ContatoController.java
@@ -1,6 +1,7 @@
 package br.com.agenda.controller;
 
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -24,9 +25,16 @@ public class ContatoController implements Serializable{
  
 	private Contato cadastrar(Contato contato) {
 		contato.setId(proximoId);
+		
 		this.contatos.put(contato.getId(), contato);
+		
+		proximoId++;
 
 		return contato;
+	}
+	
+	private Collection<Contato> buscarTodos(){
+		return contatos.values();
 	}
 
 	@RequestMapping(method=RequestMethod.POST, value="/contatos", consumes=MediaType.APPLICATION_JSON_VALUE)
@@ -34,7 +42,15 @@ public class ContatoController implements Serializable{
 	
 		Contato contatoCadastrado = this.cadastrar(contato);
 		
-		return new ResponseEntity<>(contatoCadastrado, HttpStatus.CREATED);
+		return new ResponseEntity<>(contatoCadastrado, HttpStatus.CREATED);	
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/contatos", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Collection<Contato>> buscarContatos(){
+		
+		Collection<Contato> todosContatos = this.buscarTodos();
+		
+		return new ResponseEntity<>(todosContatos, HttpStatus.OK);
 		
 	}
 
